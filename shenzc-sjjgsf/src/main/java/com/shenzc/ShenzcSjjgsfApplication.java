c170eb95f9667967a12ed3e6fc283a235f417504
shenzc <1054694244@qq.com>
2020-06-12 08:55:32 +0800

    sjjg

---
+++ shenzc-sjjgsf/src/main/java/com/shenzc/ShenzcSjjgsfApplication.java
@@ -0,0 +1,13 @@
+package com.shenzc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShenzcSjjgsfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShenzcSjjgsfApplication.class, args);
+    }
+
+}
